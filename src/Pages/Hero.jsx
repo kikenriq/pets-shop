795465333b718215444aaf13e7f82b4e74a2097c
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-07-03 12:18:52 -0400

    Starter files

---
+++ src/Pages/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Heroimg from '../assets/images/hero-banner.jpg'
+
+const Hero = () => {
+  return (
+    <div className='h-screen'>
+        <div className='h-full'>
+            <img className='h-[50%] object-cover object-left absolute' src={Heroimg} alt="" />
+            <div className='flex flex-col items-center justify-center text-center relative'>
+                <h1 className='font-[bangers] text-white text-7xl'><span className='font-[bangers] text-4xl'>High Quality</span> Pet Food</h1>
+                <p className='text-white'>Sale up to 40% off today</p>
+                <button>Shop Now</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
