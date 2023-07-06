d63b834acd01464db9980526903b2a4c8cba1976
Kikecode <108308253+kikenriq@users.noreply.github.com>
2023-07-05 22:20:22 -0400

    added cta and services pages

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div>
             <img className='h-[50%] md:w-screen md:mt-15 md:h-auto object-cover object-left absolute' src={Heroimg} alt="" />
             <div className='flex flex-col gap-2 items-center justify-center py-20 text-center relative'>
-                <h1 className='font-[bangers] text-white text-7xl'><span className='font-[bangers] text-4xl'>High Quality</span> Pet Food</h1>
+                <h1 className='font-[bangers] text-white text-7xl'><span className=' font-[bangers] text-4xl'>High Quality</span> Pet Food</h1>
                 <p className='text-white'>Sale up to 40% off today</p>
                 <button>Shop Now</button>
             </div>
