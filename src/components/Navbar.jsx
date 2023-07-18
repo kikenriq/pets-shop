02f82a964798a587141362f2eb848351aa86bb36
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-07-18 12:46:15 -0400

    New navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,52 @@
 import React from 'react'
-import { Button, Navbar } from 'flowbite-react';
+import { useState, useEffect } from 'react';
 
 
-const resNavbar = () => {
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() =>{
+    window.addEventListener('scroll', () =>{
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    })
+  })
+
   return (
-    <>
-    <Navbar
-      fluid
-      rounded
-      className='w-screen items-center px-4 md:px-48 fixed z-30 shadow-lg'
-    >
-        
-    <Navbar.Toggle className=' bg-transparent md:hidden'/>
+    <nav className={`${isActive ? 'bg-white shadow-lg' : 'bg-transparent'} fixed z-30 w-full py-4 px-48 flex justify-between` }>
+        <div className='flex gap-20'>
+          <a href="#" className={`${isActive ? 'text-black' : 'text-white'} font-[bangers] text-5xl`}>
+            Pet's Shop
+          </a>
 
-      <Navbar.Brand href="https://flowbite-react.com">
-       <a href="#">
-         <h1 className=' text-3xl font-[bangers]'>Pets's Shop</h1>
-       </a>
-      </Navbar.Brand>
+          <ul className='flex items-center gap-8'>
+            <li>
+              <a className={`${isActive ? 'text-black' : 'text-white'} font-semibold text-lg hover:text-orange-500 ease-in-out duration-200`} href="#">Home</a>
+            </li>
+            <li>
+              <a className={`${isActive ? 'text-black' : 'text-white'} font-semibold text-lg hover:text-orange-500 ease-in-out duration-200`} href="#">Shop</a>
+            </li>
+            <li>
+              <a className={`${isActive ? 'text-black' : 'text-white'} font-semibold text-lg hover:text-orange-500 ease-in-out duration-200`} href="#">Collection</a>
+            </li>
+            <li>
+              <a className={`${isActive ? 'text-black' : 'text-white'} font-semibold text-lg hover:text-orange-500 ease-in-out duration-200`} href="#">Blogs</a>
+            </li>
+            <li>
+              <a className={`${isActive ? 'text-black' : 'text-white'} font-semibold text-lg hover:text-orange-500 ease-in-out duration-200`} href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className=" md:order-2">
-        <a href="#" className='text-3xl text-gray-700'>
-            <ion-icon name="bag-handle-outline"></ion-icon>
-        </a>
-      </div>
+        <div className={`${isActive ? 'text-black' : 'text-white'} flex gap-10 items-center text-2xl`}> 
+          <a href="#">
+              <ion-icon name="bag-handle-outline"></ion-icon>
+          </a>
 
-      <Navbar.Collapse>
-        <Navbar.Link
-          active
-          href="#"
-          className=' bg-orange-400'
-        >
-          <p>
-            Home
-          </p>
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Services
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Pricing
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Contact
-        </Navbar.Link>
-      </Navbar.Collapse>
-
-    </Navbar>
-
-    </>
+          <a href="#">
+              <ion-icon name="bag-handle-outline"></ion-icon>
+          </a>
+        </div>
+    </nav>
   )
 }
-export default resNavbar
+export default Navbar
